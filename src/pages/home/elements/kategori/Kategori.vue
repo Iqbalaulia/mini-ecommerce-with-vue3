83c5a3906925d07a248f37eddff556847bdef547
iqbalaulia <iqbal.developer16@gmail.com>
2023-05-31 03:02:35 +0700

    kategori

---
+++ src/pages/home/elements/kategori/Kategori.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="category">
+    <h2>Kategori</h2>
+    <div class="content row">
+      <div class="col-md-2">
+        <a href="#" class="content__category">
+          <img class="img-fluid images-content" src="@/assets/icons/ic_rocket.png" alt="promo bank">
+          <label for="">Promo Bank</label>
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="#" class="content__category">
+          <img class="img-fluid images-content" src="@/assets/icons/ic_partner.png" alt="promo partner">
+          <label for="">Promo Partner</label>
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="#" class="content__category">
+          <img class="img-fluid images-content" src="@/assets/icons/ic_sale.png" alt="super hemat">
+          <label for="">Super Hemat</label>
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="#" class="content__category">
+          <img class="img-fluid images-content" src="@/assets/icons/ic_catalogue.png" alt="promo bank">
+          <label for="">Katalog</label>
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="#" class="content__category">
+          <img class="img-fluid images-content" src="@/assets/icons/ic_new_product.png" alt="private brand">
+          <label for="">Private Brand</label>
+        </a>
+      </div>
+      <div class="col-md-2">
+        <a href="#" class="content__category">
+          <img class="img-fluid images-content" src="@/assets/icons/ic_idea.png" alt="Super event">
+          <label for="">Super Event</label>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="information-banner container-fluid">
+  <div class="information-banner">
     <VCarousel :items-to-show="2.5" :wrap-around="true">
       <Slide v-for="(itemsCarousel, index) in dataCarousel.data.items" :key="index">
         <img class="img-fluid images-carousel" :src="itemsCarousel.images_path" :alt="itemsCarousel.name">
       </Slide>
     </VCarousel>
+    <div class="container">
+      <SectionKategori/>
+    </div>
   </div>
 </template>
 
@@ -12,4 +15,5 @@
 import { Slide } from 'vue3-carousel'
 import VCarousel from '@/components/v-carousel/Carousel.vue'
 import dataCarousel from './data/carousel.json'
+import SectionKategori from '@/pages/home/elements/kategori/Kategori.vue'
 </script>
